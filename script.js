b622af50b38002a3741e0d5ff46f63f5049b6d78
Matheus Henrique <115652962+CitrusMH@users.noreply.github.com>
2023-01-03 14:33:04 -0300

    Commit Inicial
    Primeiro Commit, com objetivo e de verificar responsividade e comportamento dos elementos.

---
+++ script.js
@@ -0,0 +1,31 @@
+// Video Playbackratio // 
+
+var video = document.getElementById("background");
+video.playbackRate = .5;
+
+// Tabs //
+
+function openTab(event, idTab) {
+    var cont = document.getElementsByTagName('section')
+    for (var i = 0; i < cont.length; i++) {
+        cont[i].style.display = 'none';
+    }
+
+    var tabs = document.getElementsByClassName('tab-btn');
+
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace('active', '');
+    }
+
+    document.getElementById(idTab).style.display = 'flex';
+    event.currentTarget.className = 'tab-btn cybr-btn active';
+}
+
+// Nav LowWidth Menu //
+
+const btnNav = document.querySelector('#btnNav');
+const divNav = document.querySelector('#ulNav');
+
+btnNav.addEventListener('click', function (){
+    divNav.classList.toggle('hidden')
+})
